2f41ba7aa6846e23338a7ebb2706d1fe3063133a
lxrdflame <tyrique.s.r@gmail.com>
2025-05-15 20:39:39 +0200

    Javascript for Design Section

--- Design/Scripts/Design.js
+++ Design/Scripts/Design.js
@@ -2,13 +2,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // Select the button element
-const readMoreBtn = document.querySelector('.sideBarButton');
-
+const ShowSideBarBtn = document.getElementById('sideBarButton');
+const sideBar = document.querySelector('.SideBar');
 // Define the function to run when the button is clicked
+let isMoved = false;
 function printShit() {
-    console.log('100'); // This logs '100' to the console
+    if (!isMoved) {
+        sideBar.style.left = '0px'; // Move right
+        isMoved = true;
+      } else {
+        sideBar.style.left = '-210px'; // Return to start
+        isMoved = false;
+      }
+}
+
+function CloseSideBar()
+{
+    if(isMoved)
+    {
+        sideBar.style.left = '-210px'; // Return to start
+        isMoved = false;
+    }
+    else {return;}
 }
 
 // Attach the function to the button's click event
-readMoreBtn.addEventListener('click', printShit);
+ShowSideBarBtn.addEventListener('click', printShit);
+window.addEventListener('scroll', CloseSideBar)
+
 });
