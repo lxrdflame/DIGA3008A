84e84d10d3cc8ce08d9afac88ee772e22b7bda46
lxrdflame <tyrique.s.r@gmail.com>
2025-05-22 17:03:31 +0200

    back to top button updates

--- Main-JavaScript/MainJavascript.js
+++ Main-JavaScript/MainJavascript.js
@@ -2,26 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const BackToTopButton = document.querySelector('.back-to-top');
 const BackToTopdiv = document.getElementById("Button-div");
+const Header = this.getElementById("header");
+
 function BackToTop(){
     document.getElementById("header").scrollIntoView();
     BackToTopdiv.style.display = "none";
 
 }
 
-let lastScrollTop = 0;
-
-function HideBacToTopButton(){
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if(scrollTop > lastScrollTop)
-    {
-        BackToTopdiv.style.display = "block";
-
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      BackToTopdiv.style.display = "none";
+      }
+     else {
+      BackToTopdiv.style.display = "block";
     }
-    lastScrollTop = scrollTop <= 0 ? 0 :scrollTop;
-}
+  });
+});
+
+
+observer.observe(Header);
 
 BackToTopButton.addEventListener('click', BackToTop);
-window.addEventListener('scroll', HideBacToTopButton);
 
 });
